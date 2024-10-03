ce6f70ceb52d5670bd78116b713f9e44c800d5a9
Yuliia Dolhopolova <dogla3@gmail.com>
2024-10-03 21:00:16 +0200

    added new exercises

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function sayHello(name) {
+    return "Hello, " + name;
+}
+// or
+function sayHello1(name) {
+    return `Hello, ${name}`;
+}
+// or arrow function-best code
+const sayHello2 = (name) => `Hello, ${name}`
+
+
 // 1
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
@@ -20,14 +31,12 @@ const goals = (a,b,c) => a + b + c;
 //Make negative //
 
 function makeNegative(num) {
-    if (num > 0) {
-        return num = -num;
-    } else {
-        return(num);
-    }
+    if (num < 0) return num;
+    return num;
 }
-// вариант codewars
-function makeNegative(num) {
+
+// вариант codewars - best
+function makeNegative1(num) {
     return num > 0 ? -num : num;
 }
 //4
@@ -39,16 +48,16 @@ function move (position, roll) {
 //5
 //Personalized Message
 function greet (name, owner) {
-    if (name === owner) {
-        return 'Hello boss'
-    }
-    else {
+    if (name === owner) return 'Hello boss';
         return 'Hello guest'
-    }
 }
 //
-function greet (name, owner) {
+function greet1 (name, owner) {
     return name === owner ? 'Hello boss' : 'Hello guest'
+}
+//
+function greet2 (name, owner) {
+    return "Hello" + (name === owner ? "boss" : "guess")
 }
 
 //6
@@ -64,3 +73,45 @@ function lovefunc(flower1, flower2){
 }
 
 
+//Convert a string to an array
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray1(string){
+    return string.split(" ")
+}
+
+//codewars
+const stringToArray = string => string.split(' ');
+
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+//DNA to RNA Conversion
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.replaceAll("T", "U")
+}
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+//Find Maximum and Minimum Values of a List
+const min1 = (list) => Math.min(...list);
+const max1 = (list) => Math.max(...list);
+
+//codewars
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+
+//Наименьшее значение массива
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
+    const minValue = Math.min(...arr)
+    if (toReturn === 'value') {
+        return minValue;
+    } else if (toReturn === 'index') {
+        return arr.indexOf(minValue);
+    }
+}
+//
+min = (arr, toReturn) => {
+    const minValue = Math.min(...arr);
+    return toReturn === 'value' ? minValue : arr.indexOf(minValue)
+};
